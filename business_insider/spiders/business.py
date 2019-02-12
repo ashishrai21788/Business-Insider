1c7cb15a47489e5f0f09aa99b0767d19faf52e6a
pranjal shrivastav <pranjalshrivastav@pranjals-Mac-mini.local>
2019-02-12 13:29:26 +0530

    initail commit

---
+++ business_insider/spiders/business.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class BusinessSpider(scrapy.Spider):
+    name = 'business'
+    allowed_domains = ['www.businessinsider.in']
+    start_urls = ['http://www.businessinsider.in/']
+
+    def parse(self, response):
+        for business_url in response.css("div.navstry-container > h3 > a ::attr(href)").extract():
+            yield scrapy.Request(response.urljoin(business_url), callback=self.parse_detail_page)
+    
+    def parse_detail_page(self, response):
+        headline = response.css("div.mobile_padding").extract_first()
+        author = response.css("span.foreign_author").extract_first()
+        
+        item = BusinessInsiderItem()
+        item['headline'] = headline
+        item['author'] = author
+        item['url'] = response.url
+        yield item
